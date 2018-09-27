6e74acf7f4a2e26c72a949232e0d011aa2c531a4
Sam <samuel.dacosta@student.manchester.ac.uk>
2018-09-27 14:02:36 +0100

    Fix error parsing and threading

--- src/main/kotlin/org/littlegit/client/engine/api/ApiCallback.kt
+++ src/main/kotlin/org/littlegit/client/engine/api/ApiCallback.kt
@@ -3,45 +3,43 @@ package org.littlegit.client.engine.api
 import org.littlegit.client.engine.model.I18nKey
 import org.littlegit.client.engine.serialization.MoshiProvider
 import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 import tornadofx.*
 
 typealias ApiCallCompletion<T> = (ApiResponse<T>) -> Unit
-class ApiCallback<T>(private val completion: ApiCallCompletion<T>) : Callback<T> {
 
-    override fun onResponse(call: Call<T>, response: Response<T>) {
-        if (response.isSuccessful) {
-            completion(ApiResponse.success(response.body()))
-        } else {
-            runAsync {
-                parseError(response)
-            } ui {
-                completion(ApiResponse.error(it))
-            }
-        }
-    }
+private fun parseError(response: Response<*>): CallFailure.ApiError? {
+    val error = response.errorBody()?.string() ?: return null
+    val moshi = find(MoshiProvider::class).moshi
 
-    override fun onFailure(call: Call<T>?, throwable: Throwable?) {
-        completion(ApiResponse.failure(throwable))
-    }
-
-    private fun parseError(response: Response<*>): CallFailure.ApiError? {
-        val error = response.errorBody()?.string() ?: return null
-        val moshi = find(MoshiProvider::class).moshi
-
-        return try {
-            moshi.adapter(CallFailure.ApiError::class.java).fromJson(error)
-        } catch (ignored: Throwable) {
-            null
-        }
+    return try {
+        val errorResponse = moshi.adapter(ErrorResponse::class.java).fromJson(error) ?: ErrorResponse()
+        CallFailure.ApiError(response.code(), errorResponse.rawMessage, errorResponse.localisedMessage, errorResponse.notes)
+    } catch (ignored: Throwable) {
+        null
     }
 }
 
 fun <T>Call<T>.enqueue(completion: ApiCallCompletion<T>) {
-    
-    this.enqueue(ApiCallback(completion))
+    runAsync {
 
+        val response: Response<T>
+
+        try {
+            response = this@enqueue.execute()
+        } catch (e: Throwable) {
+            return@runAsync ApiResponse.failure<T>(e)
+        }
+
+
+        if (response.isSuccessful) {
+            return@runAsync ApiResponse.success(response.body())
+        } else {
+            return@runAsync ApiResponse.error<T>(parseError(response))
+        }
+    } ui {
+        completion(it)
+    }
 }
 
 data class ApiResponse<T>(val isSuccess: Boolean, val body: T?, val errorBody: CallFailure?) {
@@ -52,6 +50,7 @@ data class ApiResponse<T>(val isSuccess: Boolean, val body: T?, val errorBody: C
     }
 }
 
+private data class ErrorResponse(val rawMessage: String = "", val localisedMessage: List<I18nKey> = emptyList(), val notes: List<String> = emptyList())
 sealed class CallFailure {
     data class ApiError(val errorCode: Int, val rawMessage: String, val localisedMessage: List<I18nKey>, val notes: List<String>): CallFailure()
     data class ApiFailure(val throwable: Throwable?): CallFailure()
