6eb3e512e8d236ab30348c5267381008edb93e05
Sam <samuel.dacosta@student.manchester.ac.uk>
2018-10-21 15:42:53 +0100

    WIP adding ssh keys

--- src/main/kotlin/org/littlegit/client/engine/controller/ApiController.kt
+++ src/main/kotlin/org/littlegit/client/engine/controller/ApiController.kt
@@ -1,21 +1,76 @@
 package org.littlegit.client.engine.controller
 
+import okhttp3.OkHttpClient
 import org.littlegit.client.engine.serialization.MoshiProvider
 import org.littlegit.client.engine.api.AuthApi
+import org.littlegit.client.engine.api.UserApi
 import org.littlegit.client.env.EnvSwitcher
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import tornadofx.*
+import com.sun.corba.se.spi.presentation.rmi.StubAdapter.request
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+
+
 
 class ApiController: Controller() {
 
     val authApi: AuthApi
+    val userApi: UserApi
     private val moshiProvider: MoshiProvider by inject()
+    lateinit var authController: AuthController
 
     init {
         val retrofit = buildLittlGitRetrofit()
+        val authorizedRetrofit = buildAuthorizedRetrofit()
 
         authApi = retrofit.create(AuthApi::class.java)
+        userApi = authorizedRetrofit.create(UserApi::class.java)
+    }
+
+    private fun buildAuthorizedRetrofit(): Retrofit
+            = Retrofit.Builder()
+                .baseUrl(EnvSwitcher.currentEnvironment.baseUrl)
+                .addConverterFactory(MoshiConverterFactory.create(moshiProvider.moshi))
+                .client(buildClient())
+                .build()
+
+    private fun buildClient(): OkHttpClient {
+
+        return OkHttpClient.Builder()
+                .addInterceptor(Interceptor { chain ->
+                    val tokens = authController.authTokens
+
+                    if (tokens == null || tokens.accessToken.isEmpty()) {
+                        return@Interceptor chain.proceed(chain.request())
+                    }
+
+                    val authorisedRequest = chain.request().newBuilder()
+                            .addHeader("Authorization", tokens.accessToken).build()
+
+                    val result = chain.proceed(authorisedRequest)
+
+                    if (result.code() == 401) {
+                        val refreshResponse = authController.refreshToken()
+
+                        if (refreshResponse?.isSuccess == true && refreshResponse.body?.accessToken != null) {
+                            val refreshedRequest = chain
+                                    .request()
+                                    .newBuilder()
+                                    .addHeader("Authorization", refreshResponse.body.accessToken)
+                                    .build()
+
+                            chain.proceed(refreshedRequest)
+                        } else {
+                            result
+                        }
+                    } else {
+                        result
+                    }
+
+                }).build()
     }
 
     private fun buildLittlGitRetrofit(): Retrofit
---
+++ src/main/kotlin/org/littlegit/client/engine/controller/SShController.kt
@@ -0,0 +1,76 @@
+package org.littlegit.client.engine.controller
+
+import org.littlegit.client.engine.db.SShDb
+import org.littlegit.client.engine.util.SimpleCallback
+import tornadofx.*
+import java.io.File
+import java.nio.file.Paths
+import com.jcraft.jsch.JSch
+import com.jcraft.jsch.KeyPair
+import org.littlegit.client.engine.api.enqueue
+import org.littlegit.client.engine.model.SshKeyRequest
+import org.littlegit.core.util.joinWithSpace
+import java.nio.file.Path
+
+
+class SShController: Controller() {
+
+    private val sshDb: SShDb by inject()
+    private val userApi = find(ApiController::class.java).userApi
+    private val userController: UserController by inject()
+
+    fun checkSshKeysExist(completion: SimpleCallback<Boolean>) {
+        sshDb.getSshKeyPath {  keyPath ->
+            completion(keyPath?.toFile()?.exists() == true)
+        }
+    }
+
+    fun generateAndAddSshKey() {
+
+        runAsync {
+
+            val sshPath = if (defaultSshKeysExist()) {
+                Paths.get(System.getProperty("user.dir", ".ssh"))
+            } else {
+                Paths.get(System.getProperty("user.home"), "ssh", "id_rsa")
+            }
+
+            try {
+                generateAndWrite(sshPath)
+            } catch (e: Exception) {
+
+            }
+
+            val publicKeyString = getPublicKeyPath(sshPath).readLines().joinWithSpace()
+            userApi.addSshKey(SshKeyRequest(publicKeyString, userController.currentUser?.id!!)).enqueue {
+
+            }
+
+
+        }
+
+
+    }
+
+    private fun generateAndWrite(sshPath: Path) {
+        val privatePath = getPrivateKeyPath(sshPath)
+        val publicKey = getPublicKeyPath((sshPath))
+        val jsch = JSch()
+
+        val kpair = KeyPair.genKeyPair(jsch, KeyPair.RSA)
+        kpair.writePrivateKey(privatePath.canonicalPath)
+        kpair.writePublicKey(publicKey.canonicalPath, "SSHCerts")
+        kpair.dispose()
+    }
+
+    private fun getPublicKeyPath(sshPath: Path) = sshPath.resolve("id_rsa.pub").normalize().toFile()
+    private fun getPrivateKeyPath(sshPath: Path) = sshPath.resolve("id_rsa").normalize().toFile()
+
+
+    // Check if the user already uses the default ssh keys because if they do, we'll leave them alone and put ours elsewhere
+    // However, this will mess up if they're using an old version of git since the command to specify ssh key locations via the
+    // config doesn't exist, but there's not much we can do about that
+    private fun defaultSshKeysExist(): Boolean {
+        return true//Paths.get(System.getProperty("user.home"), "ssh", "id_rsa").toFile().exists()
+    }
+}
