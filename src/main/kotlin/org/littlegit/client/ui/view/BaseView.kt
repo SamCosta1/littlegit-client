16d367c194c7df08079ec7450d9c2810b4e87c01
Sam <samuel.dacosta@student.manchester.ac.uk>
2018-11-08 11:13:10 +0000

    Stop the whole ui resizing unnecesarily

--- src/main/kotlin/org/littlegit/client/ui/view/BaseView.kt
+++ src/main/kotlin/org/littlegit/client/ui/view/BaseView.kt
@@ -5,14 +5,16 @@ import org.littlegit.client.engine.controller.RepoController
 import org.littlegit.client.engine.i18n.Localizer
 import tornadofx.*
 
-abstract class BaseView(title: String? = null, private val fullScreen: Boolean = false) : View(title ?: "") {
+abstract class BaseView(title: String? = null, private val fullScreen: Boolean? = null) : View(title ?: "") {
     protected val localizer: Localizer by inject()
     protected val repoController: RepoController by inject()
     protected val littleGitCoreController: LittleGitCoreController by inject()
 
     override fun onDock() {
         super.onDock()
-        currentStage?.isMaximized = fullScreen
+        if (fullScreen != null) {
+            currentStage?.isMaximized = fullScreen
+        }
     }
 }
 
